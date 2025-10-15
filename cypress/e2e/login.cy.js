c3b2b954f250f088529270af59a63c31e8f9148e
Gustavo Lima01 <gustahlima@hotmail.com>
2025-10-14 21:11:06 -0300

    Adicionando screenshots

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,13 +2,16 @@ describe('login', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:4000')
+    cy.screenshot('após-visitar-pagina-de-login')
   })
 
   it('Login com dados validos deve permitir entrada no sistema', () => {
     
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
+    cy,screenshot('após-preencher-dados-validos')
     cy.contains('button', 'Entrar').click()
+    cy.screenshot('após-clicar-no botao-entrar')
 
     cy.contains('h4','Realizar Transferência').should('be.visible')
   })
